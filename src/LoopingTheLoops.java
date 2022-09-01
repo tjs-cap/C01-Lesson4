4b402dc10f7271d151b55ada5887794befa6873d
TJS Capita <trevor.smith@capita.com>
2022-09-01 15:14:06 +0100

    Initial commit

---
+++ src/LoopingTheLoops.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LoopingTheLoops {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Cristina");
+//        }
+
+//        System.out.print("Enter your username: ");
+//        Scanner reader = new Scanner(System.in);
+//        String username = reader.nextLine();
+//        while (username.isEmpty()){
+//            System.out.print("Enter your username: ");
+//            username = reader.nextLine();
+//        }
+//        System.out.println(username);
+
+//        Scanner reader = new Scanner(System.in);
+//        String username;
+//        do {
+//            System.out.print("Enter your username: ");
+//            username = reader.nextLine();
+//        } while (username.isEmpty());
+//        System.out.println(username);
+    }
+}
